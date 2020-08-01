69012837b1c99236514dab8c28034cf77c8bc17d
LuisMatheus.dev <luismatheusbs.2016@gmail.com>
2020-08-01 14:33:07 -0300

    :rocket: Adicionado Rotas para Exibição e Listagem
    Utilizando de recursos nativos do node, desenvolvemos as requisições e
    respostas para a micro-api, recebendo os dados de um arquivo local

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-console.log()
+const data = fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) =>  {
+const productData = JSON.parse(data);
+
+const server = http.createServer(( req, res) => {
+    console.log(req.url)
+
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === "/overview") {
+        res.end("Você esta em Overview")
+    
+    } else if (pathName === '/product') {
+        res.end("Você esta em Produtos ")
+    
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json'})
+        res.end(productData);
+        
+    } else {
+        res.writeHead(404);
+        res.end('Page not found')
+    }
+});
+
+server.listen(3333, '127.0.0.1', () => {
+    console.log('Listening to request asdsadsa')
+})
