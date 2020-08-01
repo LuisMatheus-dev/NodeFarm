b669b8de101ce96c7ce9558cfa09ddb430999271
LuisMatheus.dev <luismatheusbs.2016@gmail.com>
2020-08-01 17:07:23 -0300

    :rocket: [feat] Rederenzido elementos dos dados
    Utlizando dos dados locais , foi criado uma função para renderizar os
    elementos do Arquivo Json , com a ajuda do metodo replace , tornando-os
    dinamicos para quaisquer futuras alterações

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 const fs = require('fs');
 const http = require('http');
-const url = require('url');
+const url = require('url'); 
 
-const data = fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) =>  {
-const productData = JSON.parse(data);
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace(/{%IMAGE%}/g,product.image);
+    output = output.replace(/{%PRICE%}/g,product.price);
+    output = output.replace(/{%FROM%}/g,product.from);
+
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g,product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g,product.description);
+    output = output.replace(/{%ID%}/g,product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProducts = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data);
+
 
 const server = http.createServer(( req, res) => {
-    console.log(req.url)
 
     const pathName = req.url;
 
-    if(pathName === '/' || pathName === "/overview") {
-        res.end("Você esta em Overview")
-    
+    if(pathName === '/' || pathName === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html'})
+        
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output);
+        
+
     } else if (pathName === '/product') {
         res.end("Você esta em Produtos ")
     
     } else if (pathName === '/api') {
-        res.writeHead(200, { 'Content-type': 'application/json'})
-        res.end(productData);
+        res.writeHead(200, { 'Content-type': 'application/json'});
+        res.end(data);
         
     } else {
-        res.writeHead(404);
-        res.end('Page not found')
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-word'
+        });
+        res.end('<h1>Page not found</h1>')
     }
 });
 
