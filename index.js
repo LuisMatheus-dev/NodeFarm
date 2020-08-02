d50502d03f4d013ceb943129b482387270ea2c3f
LuisMatheus.dev <luismatheusbs.2016@gmail.com>
2020-08-02 16:23:52 -0300

    :rocket: [refactor] Estrutura

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const replaceTemplate = (temp, product) => {
     return output;
 }
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/index.html`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/index.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
 
